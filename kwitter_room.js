d7a406c9c8ccfa95ae08cc905bd7ca4021950d36
Aadi-kashyav <101461343+Aadi-kashyav@users.noreply.github.com>
2022-08-06 16:41:52 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,4 +1,15 @@
-
+var firebaseConfig = {
+      apiKey: "AIzaSyBeo0DIkjsB4z6koKCijng58mTdGOiE4kI",
+      authDomain: "kwitter-database-49fec.firebaseapp.com",
+      databaseURL: "https://kwitter-database-49fec-default-rtdb.firebaseio.com",
+      projectId: "kwitter-database-49fec",
+      storageBucket: "kwitter-database-49fec.appspot.com",
+      messagingSenderId: "671110449308",
+      appId: "1:671110449308:web:d7c4b5f3409c3134aa4559"
+    };
+    
+    // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 //ADD YOUR FIREBASE LINKS HERE
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
